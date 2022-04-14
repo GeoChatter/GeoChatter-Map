6a921a4205910c811a3936393cd9b5d8de5145c1
Sören Michaels <soeren.michaels@gmail.com>
2022-04-14 13:55:17 +0200

    fixed build error

--- src/routes/Leaflet.svelte
+++ src/routes/Leaflet.svelte
@@ -12,11 +12,13 @@
 	import { copyAndPaste } from '$lib/Drawer.svelte';
 
 	$: copy = !$user || copyAndPaste;
-	let profileIcon = L.icon({
-		iconUrl: $user?.user_metadata?.picture ?? 'test.png',
-		iconSize: [30, 30],
-		className: 'rounded-full border-2 border-white'
-	});
+	if (browser) {
+		let profileIcon = L.icon({
+			iconUrl: $user?.user_metadata?.picture ?? 'test.png',
+			iconSize: [30, 30],
+			className: 'rounded-full border-2 border-white'
+		});
+	}
 	function initLeaflet(node) {
 		let marker;
 		let layers = {
