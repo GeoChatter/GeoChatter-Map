62036e3dd9b04ff7adff67bf5519afa6ae6c7e38
Sören Michaels <soeren.michaels@gmail.com>
2022-04-14 22:26:05 +0200

    added 3 dots to open drawer

--- src/lib/QuickSwitch.svelte
+++ src/lib/QuickSwitch.svelte
@@ -1,5 +1,6 @@
 <script>
 	import MapPicker from './MapPicker.svelte';
+	import { open } from '$lib/Drawer.svelte';
 </script>
 
 <div class="pointer-events-none absolute w-full flex justify-center z-[5000] mt-2">
@@ -7,5 +8,6 @@
 		class="pointer-events-auto menu menu-horizontal menu-compact 2xl:menu-normal bg-base-100 rounded-box"
 	>
 		<MapPicker />
+		<li on:click={() => ($open = !$open)}><a class="font-bold">...</a></li>
 	</ul>
 </div>
