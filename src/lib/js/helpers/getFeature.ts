5dccfadd3d4e2b16e27f1f73ddcb8ee41fbedcdc
Sören Michaels <soeren.michaels@gmail.com>
2022-06-04 12:41:13 +0200

    speed improvement

--- src/lib/js/helpers/getFeature.ts
+++ src/lib/js/helpers/getFeature.ts
@@ -24,9 +24,11 @@ async function downloadAndUnzip() {
 
     for (const countryFile of Object.values(loadedZip.files)) {
       try {
-        const content = await countryFile.async("string")
-        const json = JSON.parse(content)
-        result_borders.push(json)
+        (async () => {
+          const content = await countryFile.async("string")
+          const json = JSON.parse(content)
+          result_borders.push(json)
+        })()
       }
       catch (e) {
         console.log(e)
@@ -52,6 +54,7 @@ async function downloadAndUnzipFlags() {
 
     for (const countryFile of Object.values(loadedZip.files)) {
       try {
+
         const content = await countryFile.async("string")
         svgs[countryFile.name.replace(".svg", "").replace("flags/", "")] = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(content)))
       }
