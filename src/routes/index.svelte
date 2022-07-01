97a734fd7d55877edb6df45dda9bd11f3cf4231a
Sören Michaels <soeren.michaels@gmail.com>
2022-07-01 18:00:31 +0200

    removed some console logs in prod

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,15 @@
 <script>
 	import Map from '$lib/Map.svelte';
 
+	import { browser, dev } from '$app/env';
 	// import { senfFlagToClients } from '$lib/js/signalR';
 	// import { createFakeGuess } from '$lib/js/signalR.test';
+
+	if (browser && !dev) {
+		window.console.log = () => {};
+		window.console.warn = () => {};
+		window.console.error = () => {};
+	}
 </script>
 
 <main class="relative w-full h-full ">
