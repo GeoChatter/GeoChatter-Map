8f9ac50ba9ce3e631307cb912fb964e8e600d3a7
Sören Michaels <soeren.michaels@gmail.com>
2022-08-16 10:29:52 +0200

    fixed build error

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
-
   kit: {
 
     prerender: { enabled: true, default: true },
